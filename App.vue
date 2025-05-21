2fd67cd580b33cfbe4aefe74fa8c6ad9528bd90c
Vasilev Aleksey Sergeevich <140093086+VasilevAC86@users.noreply.github.com>
2025-05-21 20:26:29 +0500

    Add files via upload

--- App.vue
+++ App.vue
@@ -3,6 +3,12 @@
   <input v-model="height" placeholder="высота" type="text">
 
   <div>Площадь прямоугольника: <input v-model="square" /></div> <!--Для вывода площади используем вычисляемое свойство, для записи - input-->
+  <button @click="activate">Ok</button>  <!--@ - это сокращение v-on-->
+  <div :class="{box:true, active: isActive}"></div>
+
+  <ul>
+    <li v-bind:key="i" v-for="item, i of list">{{ item }}</li> <!--перебор через i для правильного перебора массива, отрисовка изменяемых в массиве элементов-->
+  </ul>
 </template>
 
 <script>
@@ -11,7 +17,10 @@
     data() { // Состояние компонента, метод, который возвращает начальное состояние объекта
       return{
         width: 10,
-        height: 10
+        height: 10,
+
+        isActive: false,
+        list: ['первый', 'второй', 'третий']
       }
     },    
     computed: { // Вычисляемое свойство (площадь). Должно использовать хотя бы одно поле из состояния компонента data(){}      
@@ -20,11 +29,18 @@
       // }
       square: { // чтение и запись
         get() {return this.width * this.height},
-        set(v) { this.width = Math.sqrt(v), this.height = Math.sqrt(v)} // v - то, чтов input площади
+        set(v) { this.width = Math.sqrt(v), this.height = Math.sqrt(v)} // v - то, что пользователь внёс в input площади
       }
     },
     methods: { // функции компонента
-
+      activate(){
+        this.isActive = !this.isActive;
+      }
+    },
+    watch: { // Объект-наблюдатель за isActive
+      isActive(oldV, newV){ 
+        console.log(`старое значение: ${oldV}, новое: ${newV}`);
+      }
     },
     props: { // входные параметры компонета, может быть массивом, каждый эл. которого - параметр
       type: String, // ограничиваем типом данных
@@ -47,5 +63,12 @@
 </script>
 
 <style scoped>
-
+  .box{
+    width: 50px;
+    height: 50px;
+    border: 2px solid black;
+  }
+  .active{
+    background-color: aqua;
+  }
 </style>
